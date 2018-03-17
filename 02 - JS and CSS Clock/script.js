87a2900b42035850022f5954bb149cd8f2cfe420
Alexander Tatarinov <tatarinov.alexander@gmail.com>
2018-03-17 12:16:36 -0400

    finish clock

---
+++ 02 - JS and CSS Clock/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const secondHand = document.querySelector('.second-hand');
+const minHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const mins = now.getMinutes();
+  const hrs = now.getHours();
+
+  const secondsDegrees = ((seconds / 60) * 360) + 90;
+  const minsDegrees = ((mins / 60) * 360) + 90;
+  const hrsDegrees = ((hrs / 12) * 360) + 90;
+
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minHand.style.transform = `rotate(${minsDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hrsDegrees}deg)`;
+}
+setDate();
+
+setInterval(setDate, 1000);
